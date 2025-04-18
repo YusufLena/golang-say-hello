1afbe5fbf2b7d1fdddb098238cf23a35829ddd41
YusufLena <sealtilena12@gmail.com>
2025-04-18 19:23:52 +0700

    update say_hello

--- say_hello.go
+++ say_hello.go
@@ -2,24 +2,33 @@ package golangsayhello
 
 import "fmt"
 
-func endApp(){
-	fmt.Println("menutup apk")
+type Age func(int) int
+type Name func(string) string
+func Hello(name string, filterName Name, age int, filterAge Age){
+	nama := filterName(name)
+	umur := filterAge(age)
+	fmt.Println("Welcome and hello Mr", nama, "Umur anda", filterAge, ".", umur)
 }
 
-func SayHello(name string) string {
-	defer endApp()
-	return "Hello" + name
-}
-
-func Iterasi(name string) string{
-	defer endApp()
-	if name != "" {
-		for i := 0; i < len(name); i++ {
-			fmt.Println("Welcome", name)
-			break
-		}
-	} else{
-		fmt.Println("Name not found")
+func NameFilter(name string) string {
+	if name == "Anjing"{
+		return "Nama yang anda buat sangat tidak merupakan hewan"
+	} else if name == "Babi" {
+		return "Nama yang anda buat sangat tidak merupakan hewan"
+	} else if name == "Goblok" {
+		return "Nama mengandung kata kasar"
+	} else {
+		return(name)
 	}
-	return name
 }
+
+func AgeCheked(umur int) string {
+	if umur < 18 {
+		return "Umur anda Belum cukup Mr" 
+	} else if umur == 18 {
+		return "Oke umur anda pas sekali"
+	} else {
+		return "umur anda sudah sangat pas"
+	}
+}
+
