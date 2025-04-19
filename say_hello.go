485e6ad59894dc14e9e8a6eab4adfe40a6a4c312
YusufLena <sealtilena12@gmail.com>
2025-04-19 18:18:36 +0700

    update

--- say_hello.go
+++ say_hello.go
@@ -22,8 +22,8 @@ func ageCheked(age int) string {
 	}
 }
 
-func SayHello(name string, age int)(string, string){
+func SayHello(name string, age int)(string,string,string,string,string){
 	filter1 := nameFilter(name)
 	filter2 := ageCheked(age)
-	return filter1, filter2 + name
+	return "Welcome",name, "\n", filter1, filter2 + name
 }
